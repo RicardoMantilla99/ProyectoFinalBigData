88f57c337e256d16e4177df4f6f87fec2c908102
Ricardo Mantilla <jrmantillap@utn.edu.ec>
2024-01-22 01:59:46 -0500

    Tu mensaje de commit aquí

--- app.py
+++ app.py
@@ -3,18 +3,15 @@ from model import clasificar_imagen
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/clasificar', methods=['POST'])
 def clasificar():
     imagen = request.files['imagen']
-    resultado = clasificar_imagen(imagen)
-    return render_template('resultado.html', resultado=resultado)
-
+    resultado_clase = clasificar_imagen(imagen)
+    return render_template('resultado.html', resultado=resultado_clase)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
